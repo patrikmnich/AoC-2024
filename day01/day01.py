6300795d1aefce8311bea04b78863c40291b34a7
Patrik Mních <mnich.patrik@gmail.com>
2024-12-01 22:11:01 +0100

    Day 1 solution

---
+++ day01/day01.py
@@ -0,0 +1,13 @@
+with open("day01.txt", "r") as f:
+    contents = f.readlines()
+
+list1 = sorted([int(line.split(" ")[0]) for line in contents])
+list2 = sorted([int(line.strip().split(" ")[-1]) for line in contents])
+
+# Part 1
+result1 = sum([abs(n1 - n2) for n1, n2 in zip(list1, list2)])
+print(result1)
+
+# Part 2
+result2 = sum([n * list2.count(n) for n in list1])
+print(result2)
